921c5bb037584953f272b6725b968a16858d7415
h0rzn <hrzn@protonmail.com>
2023-05-24 23:58:58 +0200

    cleanup old files

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -1,5 +1,3 @@
-
-
 <template>
     <svg id="spinner" width="24" height="24" stroke="#000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <g class="spinner_V8m1"><circle class="circle" cx="12" cy="12" r="9.5" fill="none" stroke-width="3"></circle></g>
